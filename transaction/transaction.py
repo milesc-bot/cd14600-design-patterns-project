d76e9b9eee3104a47e26550447072325f439c3ee
Miles Cuban <miles.c@ponderfit.com>
2026-02-09 13:27:21 -0500

    Add transaction/transaction.py

---
+++ transaction/transaction.py
@@ -0,0 +1,17 @@
+from transaction.transaction_category import TransactionCategory
+
+
+class Transaction:
+    """Represents a financial transaction with an amount and category."""
+
+    def __init__(self, amount, category: TransactionCategory):
+        self.amount = amount
+        self.category = category
+
+    def __str__(self):
+        return f"Transaction(${self.amount}, category='{self.category}')"
+
+    def __eq__(self, other):
+        if not isinstance(other, Transaction):
+            return False
+        return self.amount == other.amount and self.category == other.category
